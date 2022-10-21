e6ca8707a50bdd5a6cb1f6983e3961f3d8716421
Olga <olga@gorchichko.com>
2022-10-21 12:13:24 +0200

    handling Input, working with list

--- src/App.js
+++ src/App.js
@@ -20,30 +20,32 @@ class App extends Component {
     })
   }
 
-  render() {
-    const cars = this.state.cars;
+  handleInput = (event) => {
+    this.setState({
+      pageTitle: event.target.value
+    })
+  }
 
+  render() {
     return (
       <div className='App'>
         <header className="App-header">
          <img src={logo} className="App-logo" alt="logo" />
           <h1>{this.state.pageTitle}</h1>
+
+          <input type="text" onChange={this.handleInput}/>
           
-          <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button>  
-
-          <Car 
-            name={cars[0].name} 
-            year={cars[0].year}
-            onChangeTitle={() => this.changeTitleHandler(cars[0].name)} />
-          <Car 
-            name={cars[1].name} 
-            year={cars[1].year}
-            onChangeTitle={() => this.changeTitleHandler(cars[1].name)} />
-          <Car 
-            name={cars[2].name} 
-            year={cars[2].year}
-            onChangeTitle={() => this.changeTitleHandler(cars[2].name)} />
-
+          <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button>
+          
+          { this.state.cars.map((car, index) => {
+            return (
+              <Car
+                key={index}
+                name={car.name} 
+                year={car.year}
+                onChangeTitle={() => this.changeTitleHandler(car.name)} />
+            )
+          })}
        </header>
       </div>
     );
