60b3bc35dc59177c0db93f98ca4699a0c2f30134
Olga <olga@gorchichko.com>
2022-10-25 15:02:09 +0200

    Using Refs

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -4,6 +4,19 @@ import withClass from "../hoc/withClass";
 import PropTypes from 'prop-types';
 
 class Car extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.inputRef = React.createRef();
+    }
+
+    componentDidMount() {
+        if (this.props.index === 1) {
+            this.inputRef.current.focus();
+        }
+    }
+
     render() {
         const inputClasses = [classes.input];
 
@@ -22,6 +35,7 @@ class Car extends Component {
                 <h3>Car name: {this.props.name}</h3>
                 <p>Year: <strong>{this.props.year}</strong></p>
                 <input 
+                    ref={this.inputRef}
                     type="text"
                     onChange={this.props.onChangeName}
                     value={this.props.name}
@@ -36,6 +50,7 @@ class Car extends Component {
 Car.propTypes = {
     name: PropTypes.string.isRequired,
     year: PropTypes.number,
+    index: PropTypes.number,
     onChangeName: PropTypes.func,
     onDelete: PropTypes.func
 }
